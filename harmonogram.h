ad0194be3f2c21032287b170501ca43916405633
GrzegorzMalolepszy <smallbetter@gmail.com>
2014-12-03 20:49:46 +0100

    Update harmonogram.h

--- harmonogram.h
+++ harmonogram.h
@@ -7,17 +7,27 @@ class harmonogram
 {
 private:
 protected:
-     int czas_wykonania_projektu;
+     int czas_realizacji;
      std::string nazwa;
 public:
      harmonogram()
      {
-          czas_wykonania_projektu = 0;
-          nazwa = "";
+          czas_realizacji = 0;
+          nazwa = "harmonogram";
+     }
+     void UstawCzasRealizacji(int czas_realizacji_projektu)
+     {
+          czas_realizacji = czas_realizacji_projektu;
+     }
+     void UstawNazwe(const char *nazwa_harmonogramu)
+     {
+          nazwa = nazwa_harmonogramu;
+     }
+     int PobierzCzasRealizacji()
+     {
+          return czas_realizacji;
      }
      virtual bool TworzHarmonogram(chromosom_zadan &chr_zadania,
-                                   chromosom_zasobow &chr_zasobu,
-                                   const char *nazwa_harmonogramu,
-                                   int czas_wykonania) = 0;
+                                   chromosom_zasobow &chr_zasobu) = 0;
 };
 #endif
